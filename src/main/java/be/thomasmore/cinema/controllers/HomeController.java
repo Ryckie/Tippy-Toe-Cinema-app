f7b05af57d183ecbc0fcbdefba92a8188c3c99ce
Ryckie <63733138+Ryckie@users.noreply.github.com>
2026-02-15 18:48:53 +0100

    Added templatepage for films

--- src/main/java/be/thomasmore/cinema/controllers/HomeController.java
+++ src/main/java/be/thomasmore/cinema/controllers/HomeController.java
@@ -15,4 +15,9 @@ public class HomeController {
     public String bioscopen() {
         return "bioscopen";
     }
+
+    @GetMapping({"films"})
+    public String films() {
+        return "films";
+    }
 }
