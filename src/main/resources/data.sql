9817ce3e0711d4e5d4a5dcbeb1d830c092ad7241
Ryckie <63733138+Ryckie@users.noreply.github.com>
2026-02-23 22:31:01 +0100

    Added 5 more movies and converted all cover pictures to webp format storing them locally.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,7 +3,7 @@ INSERT INTO MOVIE (NUMBER_OF_FREE_SEATS, RELEASE_YEAR, GENRE, HALL_NAME, MOVIE_D
 VALUES (250, 2024, 'Action/Comedy', 'B',
         'Deadpool krijgt een plaats aangeboden in het Marvel Cinematic Universe door de Time Variance Authority, maar werft in plaats daarvan een variant van Wolverine aan om zijn universum te redden van uitsterven.',
         'Deadpool & Wolverine',
-        'https://m.media-amazon.com/images/M/MV5BZTk5ODY0MmQtMzA3Ni00NGY1LThiYzItZThiNjFiNDM4MTM3XkEyXkFqcGc@._V1_.jpg',
+        '/assets/posters/deadpool-wolverine.webp',
         'https://www.youtube.com/watch?v=73_1biulkYk');
 
 INSERT INTO MOVIE (NUMBER_OF_FREE_SEATS, RELEASE_YEAR, GENRE, HALL_NAME, MOVIE_DESCRIPTION, MOVIE_NAME, POSTER_URL,
@@ -11,7 +11,7 @@ INSERT INTO MOVIE (NUMBER_OF_FREE_SEATS, RELEASE_YEAR, GENRE, HALL_NAME, MOVIE_D
 VALUES (150, 2014, 'Adventure, Drama, Sci-Fi', 'A',
         'When Earth becomes uninhabitable, a farmer and ex-NASA pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team of researchers, to find a new planet for humans.',
         'Interstellar',
-        'https://upload.wikimedia.org/wikipedia/en/b/bc/Interstellar_film_poster.jpg',
+        '/assets/posters/interstellar.webp',
         'https://www.youtube.com/watch?v=zSWdZVtXT7E');
 
 INSERT INTO MOVIE (NUMBER_OF_FREE_SEATS, RELEASE_YEAR, GENRE, HALL_NAME, MOVIE_DESCRIPTION, MOVIE_NAME, POSTER_URL,
@@ -19,14 +19,14 @@ INSERT INTO MOVIE (NUMBER_OF_FREE_SEATS, RELEASE_YEAR, GENRE, HALL_NAME, MOVIE_D
 VALUES (200, 2008, 'Action, Crime, Drama', 'C',
         'When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.',
         'The Dark Knight',
-        'https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_.jpg',
+        '/assets/posters/the-dark-knight.webp',
         'https://www.youtube.com/watch?v=EXeTwQWrcwY');
 
 INSERT INTO MOVIE (NUMBER_OF_FREE_SEATS, RELEASE_YEAR, GENRE, HALL_NAME, MOVIE_DESCRIPTION, MOVIE_NAME, POSTER_URL,
                    TRAILER_URL)
 VALUES (180, 2010, 'Action, Adventure, Sci-Fi', 'A',
         'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.',
-        'Inception', 'https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_.jpg',
+        'Inception', '/assets/posters/inception.webp',
         'https://www.youtube.com/watch?v=YoHD9XEInc0');
 
 INSERT INTO MOVIE (NUMBER_OF_FREE_SEATS, RELEASE_YEAR, GENRE, HALL_NAME, MOVIE_DESCRIPTION, MOVIE_NAME, POSTER_URL,
@@ -34,5 +34,45 @@ INSERT INTO MOVIE (NUMBER_OF_FREE_SEATS, RELEASE_YEAR, GENRE, HALL_NAME, MOVIE_D
 VALUES (120, 2023, 'Biography, Drama, History', 'B',
         'The story of American scientist, J. Robert Oppenheimer, and his role in the development of the atomic bomb.',
         'Oppenheimer',
-        'https://upload.wikimedia.org/wikipedia/en/4/4a/Oppenheimer_%28film%29.jpg',
+        '/assets/posters/oppenheimer.webp',
         'https://www.youtube.com/watch?v=uYPbbksJxIg');
+
+INSERT INTO MOVIE (NUMBER_OF_FREE_SEATS, RELEASE_YEAR, GENRE, HALL_NAME, MOVIE_DESCRIPTION, MOVIE_NAME, POSTER_URL,
+                   TRAILER_URL)
+VALUES (180, 1999, 'Action, Sci-Fi', 'A',
+        'A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.',
+        'The Matrix',
+        '/assets/posters/the-matrix.webp',
+        'https://www.youtube.com/watch?v=1HBE1CAarkU');
+
+INSERT INTO MOVIE (NUMBER_OF_FREE_SEATS, RELEASE_YEAR, GENRE, HALL_NAME, MOVIE_DESCRIPTION, MOVIE_NAME, POSTER_URL,
+                   TRAILER_URL)
+VALUES (140, 1994, 'Crime, Drama', 'C',
+        'The lives of two mob hit men, a boxer, a gangster''s wife, and a pair of diner bandits intertwine in four tales of violence and redemption.',
+        'Pulp Fiction',
+        '/assets/posters/pulp-fiction.webp',
+        'https://www.youtube.com/watch?v=HmH9-gOL4es');
+
+INSERT INTO MOVIE (NUMBER_OF_FREE_SEATS, RELEASE_YEAR, GENRE, HALL_NAME, MOVIE_DESCRIPTION, MOVIE_NAME, POSTER_URL,
+                   TRAILER_URL)
+VALUES (220, 2023, 'Animation, Action, Adventure', 'B',
+        'Miles Morales catapults across the Multiverse, where he encounters a team of Spider-People charged with protecting its very existence.',
+        'Spider-Man: Across the Spider-Verse',
+        '/assets/posters/spider-man-across-the-spider-verse.webp',
+        'https://www.youtube.com/watch?v=_adt_ESwZag');
+
+INSERT INTO MOVIE (NUMBER_OF_FREE_SEATS, RELEASE_YEAR, GENRE, HALL_NAME, MOVIE_DESCRIPTION, MOVIE_NAME, POSTER_URL,
+                   TRAILER_URL)
+VALUES (280, 2024, 'Action, Adventure, Sci-Fi', 'A',
+        'Paul Atreides unites with Chani and the Fremen while seeking revenge against the conspirators who destroyed his family.',
+        'Dune: Part Two',
+        '/assets/posters/dune-part-two.webp',
+        'https://www.youtube.com/watch?v=yu06bU0X8ug');
+
+INSERT INTO MOVIE (NUMBER_OF_FREE_SEATS, RELEASE_YEAR, GENRE, HALL_NAME, MOVIE_DESCRIPTION, MOVIE_NAME, POSTER_URL,
+                   TRAILER_URL)
+VALUES (100, 1994, 'Drama', 'C',
+        'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.',
+        'The Shawshank Redemption',
+        '/assets/posters/the-shawshank-redemption.webp',
+        'https://www.youtube.com/watch?v=rNKKgsuIZC8');
