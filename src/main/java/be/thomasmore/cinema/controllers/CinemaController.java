ed8aacae71cc2ac110eb78f29823d6e3b140db5c
Ryckie <63733138+Ryckie@users.noreply.github.com>
2026-02-23 23:54:43 +0100

    Added cinemas and cinema details per bioscoop

--- src/main/java/be/thomasmore/cinema/controllers/CinemaController.java
+++ src/main/java/be/thomasmore/cinema/controllers/CinemaController.java
@@ -6,6 +6,9 @@ import be.thomasmore.cinema.repositories.cinemaRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Optional;
 
 @Controller
 public class CinemaController {
@@ -20,6 +23,22 @@ public class CinemaController {
         Iterable<Cinema> cinemas = cinemaRepository.findAll();
         model.addAttribute("cinemas", cinemas);
 
-        return "bioscopen";
+        return "cinemas";
+    }
+
+    @GetMapping({"/bioscoop/{id}", "/bioscoop"})
+    public String bioscoop(Model model, @PathVariable(required = false) Integer id) {
+        if (id == null) {
+            Iterable<Cinema> cinemas = cinemaRepository.findAll();
+            model.addAttribute("cinemas", cinemas);
+            return "cinemas";
+        }
+        Optional<Cinema> cinemaFromDb = cinemaRepository.findById(id);
+        if (cinemaFromDb.isPresent()) {
+            model.addAttribute("cinema", cinemaFromDb.get());
+            return "cinema";
+        }
+
+        return "redirect:/bioscopen"; // https://docs.spring.io/spring-framework/reference/web/webmvc/mvc-controller/ann-methods/redirecting-passing-data.html#page-title
     }
 }
